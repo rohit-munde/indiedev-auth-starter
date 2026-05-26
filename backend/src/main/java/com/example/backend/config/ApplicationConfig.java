8a6704206ceccdafd01170d88aedc7b8ff5ea0db
Rohit Munde <rohitvmunde@gmail.com>
2026-05-27 00:59:07 +0530

    fixed deprecated functions

--- backend/src/main/java/com/example/backend/config/ApplicationConfig.java
+++ backend/src/main/java/com/example/backend/config/ApplicationConfig.java
@@ -25,8 +25,7 @@ public class ApplicationConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
     }
